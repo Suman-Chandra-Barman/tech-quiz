f6f66357d004ff5699f2a51917ed37f8eb08af00
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2022-10-11 18:10:53 +0600

    barchart done

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+} from "recharts";
 
 const Statistics = () => {
   const quizData = useLoaderData();
-  return <div>{quizData.data.map((quiz) => console.log(quiz.total))}</div>;
+  console.log(quizData.data);
+
+  return (
+    <div className="w-1/2 mx-auto">
+      <BarChart width={500} height={300} data={quizData.data}>
+        <Bar dataKey="total"></Bar>
+        <XAxis dataKey="name"></XAxis>
+        <YAxis></YAxis>
+        <Legend />
+        <Tooltip />
+      </BarChart>
+    </div>
+  );
 };
 
 export default Statistics;
